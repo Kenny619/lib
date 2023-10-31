f087cb378b0e834b40979dcc881a3cb71446ae2a
Kenny619 <ken.onishi@umusic.com>
2023-10-31 18:34:28 +0900

    Add files via upload

--- file.js
+++ file.js
@@ -11,7 +11,7 @@ const path = require("path");
  * @throws {Error} If the source directory doesn't exist or if the destination is the same as the source.
  */
 
-async function moveFiles(srcDir, destDir, regExp = false, mode = copy) {
+function moveFiles(srcDir, destDir, regExp = false, mode = "copy") {
   if (!fs.existsSync(srcDir)) throw new Error(`⛔ dir ${srcDir} does not exist.`);
   if (srcDir === destDir) throw new Error(`⛔ Destination is pointing to the source directory.`);
   //create destDir
@@ -22,8 +22,6 @@ async function moveFiles(srcDir, destDir, regExp = false, mode = copy) {
       if (err) throw new Error(`⛔ Failed to read files from ${destDir}\r\n${err}`);
 
       files.forEach(file => {
-        if (file.isDirectory()) return false;
-
         if (regExp) {
           if (!regExp.test(file.name)) return false; //exlude from copy process.
         }
@@ -31,6 +29,12 @@ async function moveFiles(srcDir, destDir, regExp = false, mode = copy) {
         const subDir = file.path.replace(srcDir, "");
         const srcFilePath = path.join(file.path, file.name);
         const destFilePath = path.join(destDir, subDir, file.name);
+
+        if (file.isDirectory()) {
+          if (!fs.existsSync(path.join(destDir, subDir))) fs.mkdirSync(path.join(destDir, subDir));
+          return false;
+        }
+
         if (!fs.existsSync(path.join(destDir, subDir))) fs.mkdirSync(path.join(destDir, subDir));
 
         fs.copyFile(srcFilePath, destFilePath, err => {
@@ -42,11 +46,9 @@ async function moveFiles(srcDir, destDir, regExp = false, mode = copy) {
             fs.unlink(srcFilePath, err => {
               if (err) console.log(`⚠ Failed to delete file ${file}`);
               console.log(`✅ ${operation} ${srcFilePath}`);
-              return true;
             });
           } else {
             console.log(`✅ ${operation} ${srcFilePath}`);
-            return true;
           }
         });
       });
