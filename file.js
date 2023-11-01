d0f933fd7883a9d4ed7e1569d50f831bd6fe2394
Kenny619 <kenny619@gmail.com>
2023-11-01 17:46:24 +0900

    file-promise completed

--- file.js
+++ file.js
@@ -12,47 +12,61 @@ const path = require("path");
  */
 
 function moveFiles(srcDir, destDir, regExp = false, mode = "copy") {
-  if (!fs.existsSync(srcDir)) throw new Error(`⛔ dir ${srcDir} does not exist.`);
-  if (srcDir === destDir) throw new Error(`⛔ Destination is pointing to the source directory.`);
+  let counter = 0;
+  if (!fs.existsSync(srcDir))
+    throw new Error(`⛔ dir ${srcDir} does not exist.`);
+  if (srcDir === destDir)
+    throw new Error(`⛔ Destination is pointing to the source directory.`);
   //create destDir
-  fs.mkdir(destDir, { recursive: true }, err => {
+  fs.mkdir(destDir, { recursive: true }, (err) => {
     if (err) throw new Error(`⛔ Failed to create dir ${destDir}\r\n${err}`);
 
-    fs.readdir(srcDir, { withFileTypes: true, recursive: true }, (err, files) => {
-      if (err) throw new Error(`⛔ Failed to read files from ${destDir}\r\n${err}`);
+    fs.readdir(
+      srcDir,
+      { withFileTypes: true, recursive: true },
+      (err, files) => {
+        if (err)
+          throw new Error(`⛔ Failed to read files from ${destDir}\r\n${err}`);
 
-      files.forEach(file => {
-        if (regExp) {
-          if (!regExp.test(file.name)) return false; //exlude from copy process.
-        }
-
-        const subDir = file.path.replace(srcDir, "");
-        const srcFilePath = path.join(file.path, file.name);
-        const destFilePath = path.join(destDir, subDir, file.name);
-
-        if (file.isDirectory()) {
-          if (!fs.existsSync(path.join(destDir, subDir))) fs.mkdirSync(path.join(destDir, subDir));
-          return false;
-        }
-
-        if (!fs.existsSync(path.join(destDir, subDir))) fs.mkdirSync(path.join(destDir, subDir));
-
-        fs.copyFile(srcFilePath, destFilePath, err => {
-          if (err) throw new Error(`⛔ Failed to copy file \r\nFROM:${srcFilePath}\r\nTO:${destFilePath}\r\n${err}`);
-
-          const operation = mode === "copy" ? "copied" : "moved";
-
-          if (mode === "cut") {
-            fs.unlink(srcFilePath, err => {
-              if (err) console.log(`⚠ Failed to delete file ${file}`);
-              console.log(`✅ ${operation} ${srcFilePath}`);
-            });
-          } else {
-            console.log(`✅ ${operation} ${srcFilePath}`);
+        files.forEach((file) => {
+          if (regExp) {
+            if (!regExp.test(file.name)) return false; //exlude from copy process.
           }
+
+          const subDir = file.path.replace(srcDir, "");
+          const srcFilePath = path.join(file.path, file.name);
+          const destFilePath = path.join(destDir, subDir, file.name);
+
+          if (file.isDirectory()) {
+            if (!fs.existsSync(path.join(destDir, subDir)))
+              fs.mkdirSync(path.join(destDir, subDir));
+            return false;
+          }
+
+          if (!fs.existsSync(path.join(destDir, subDir)))
+            fs.mkdirSync(path.join(destDir, subDir));
+
+          fs.copyFile(srcFilePath, destFilePath, (err) => {
+            if (err)
+              throw new Error(
+                `⛔ Failed to copy file \r\nFROM:${srcFilePath}\r\nTO:${destFilePath}\r\n${err}`
+              );
+            counter++;
+
+            const operation = mode === "copy" ? "copied" : "moved";
+
+            if (mode === "cut") {
+              fs.unlink(srcFilePath, (err) => {
+                if (err) console.log(`⚠ Failed to delete file ${file}`);
+                //  console.log(`✅ ${operation} ${srcFilePath}`);
+              });
+            } else {
+              //console.log(`✅ ${operation} ${srcFilePath}`);
+            }
+          });
         });
-      });
-    });
+      }
+    );
   });
 }
 module.exports = moveFiles;
